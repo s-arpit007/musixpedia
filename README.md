b6d8b32f4a1bd12ef8eed33baed89a1e45515256
sharma-arpit <s.arpit9656@gmail.com>
2021-06-14 13:56:15 +0530

    README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# musixpedia
+## musixpedia
+
+This is a utility app for Spotify with personalised recommendations based on our algorithm for song recommendation.
+This will also provide you different kind of insights into your listening behaviour.
---
+++ my_playlist.py
@@ -0,0 +1,9 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+scope = 'playlist-read-private'
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+results = sp.current_user_playlists(limit=50)
+for i, item in enumerate(results['items']):
+    print("%d %s" % (i, item['name']))
